e05b59a2dd2fdbd885ad40936c31d3d0ec69bedf
Unknown <coworking.shostka@gmail.com>
2018-03-03 15:17:34 +0200

    Home component

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
           <div class="container">
             <div class="navbar-brand">
               <a class="navbar-item" href="../">
-              <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo">
+              <img src="../static/img/it.jpg" alt="Logo">
             </a>
             <span class="navbar-burger burger" data-target="navbarMenu"
               :class="{'is-active': isActive}" @click="isActive = !isActive">
@@ -35,11 +35,11 @@
                 Documentation
               </a>
               <span class="navbar-item">
-                <a class="button is-white is-outlined is-small" href="https://github.com/dansup/bulma-templates/blob/master/templates/landing.html">
+                <a class="button is-white is-outlined is-small" href="https://www.facebook.com/CoWorking.Shostka/">
                   <span class="icon">
-                    <i class="fa fa-github"></i>
+                    <img src="../static/img/facebook.svg"/>
                   </span>
-                  <span>View Source</span>
+                  <span>Facebook</span>
                 </a>
               </span>
             </div>
@@ -48,32 +48,7 @@
         </nav>
       </div>
 
-      <div class="hero-body">
-        <div class="container has-text-centered">
-        <div class="column is-6 is-offset-3">
-          <h1 class="title">
-            Coming Soon
-          </h1>
-          <h2 class="subtitle">
-             $this is the best software platform for running an internet business. We handle billions of dollars every year for forward-thinking businesses around the world.
-          </h2>
-          <div class="box">
-            
-            <div class="field is-grouped">
-              <p class="control is-expanded">
-                <input class="input" type="text" placeholder="Enter your email">
-              </p>
-              <p class="control">
-                <a class="button is-info">
-                  Notify Me
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-
+      <router-view></router-view>
     </section>
   </div>
 </template>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hero-body">
+        <div class="container has-text-centered">
+        <div class="column is-6 is-offset-3">
+          <h1 class="title">
+            Сайт у розробці
+          </h1>
+          <h2 class="subtitle">
+            Гурток для обдарованої молоді Шостки
+          </h2>
+          <div class="box">
+            
+            <div class="field is-grouped">
+              <p class="control is-expanded">
+                <input class="input" type="text" placeholder="Enter your email">
+              </p>
+              <p class="control">
+                <a class="button is-info">
+                  Повідом Мене
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
