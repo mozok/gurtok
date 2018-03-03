0aad7782fe1c24cc192dc47874771bb425b5c2a2
Unknown <coworking.shostka@gmail.com>
2018-03-03 15:04:02 +0200

    Landing page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,89 @@
 <template>
   <div id="app">
-    <header>
+    <!-- <header>
       <span>Vue.js PWA</span>
     </header>
     <main>
       <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
-    </main>
+    </main> -->
+
+    <section class="hero is-dark is-fullheight">
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item" href="../">
+              <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu"
+              :class="{'is-active': isActive}" @click="isActive = !isActive">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            </div>
+            <div id="navbarMenu" class="navbar-menu" :class="{'is-active': isActive}">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <a class="navbar-item">
+                Examples
+              </a>
+              <a class="navbar-item">
+                Documentation
+              </a>
+              <span class="navbar-item">
+                <a class="button is-white is-outlined is-small" href="https://github.com/dansup/bulma-templates/blob/master/templates/landing.html">
+                  <span class="icon">
+                    <i class="fa fa-github"></i>
+                  </span>
+                  <span>View Source</span>
+                </a>
+              </span>
+            </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+        <div class="column is-6 is-offset-3">
+          <h1 class="title">
+            Coming Soon
+          </h1>
+          <h2 class="subtitle">
+             $this is the best software platform for running an internet business. We handle billions of dollars every year for forward-thinking businesses around the world.
+          </h2>
+          <div class="box">
+            
+            <div class="field is-grouped">
+              <p class="control is-expanded">
+                <input class="input" type="text" placeholder="Enter your email">
+              </p>
+              <p class="control">
+                <a class="button is-info">
+                  Notify Me
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {isActive: false}
+  }
 }
 </script>
 
@@ -25,10 +96,10 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: whitesmoke;
 }
 
-main {
+/* main {
   text-align: center;
   margin-top: 40px;
 }
@@ -50,5 +121,24 @@ header span {
   font-weight: 400;
   box-sizing: border-box;
   padding-top: 16px;
+} */
+
+.hero.is-dark {
+  background: linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    ), url('https://unsplash.it/g/1200/900?random') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.hero .nav, .hero.is-success .nav {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.hero .subtitle {
+  padding: 3rem 0;
+  line-height: 1.5;
 }
 </style>
